27e21e34cc733796b62b96f86f7c6874a7767ec8
Phillip <Phillip@DESKTOP-DC2065K>
2019-07-15 01:19:53 -0500

    maybe final

--- Insurance Web/CRMService.cs
+++ Insurance Web/CRMService.cs
@@ -1,5 +1,6 @@
 ﻿using Insurance_Web.Class;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,8 @@ namespace Insurance_Web
             service = new CrmServiceClient(crmConnectionString);
         }
 
-        public Entity CreateContact(Contact contact)
+        //creates a contact entity and adds it to the crm
+        public Entity CreateContactEntity(Contact contact, out Guid c_guid)
         {
             //create an entity by entity id
             Entity contactEntity = new Entity("contact");
@@ -29,9 +31,9 @@ namespace Insurance_Web
             contactEntity.Attributes.Add("firstname", contact.firstName);
             contactEntity.Attributes.Add("lastname", contact.lastName);
             //contactEntity.Attributes.Add("birthdate", contact.birthday);
-            //contactEntity.Attributes.Add("new_ssn", contact.social);
-            //contactEntity.Attributes.Add("gendercode", contact.);
-            //contactEntity.Attributes.Add("familystatuscode",contact.);
+            contactEntity.Attributes.Add("new_ssn", contact.social);
+            contactEntity.Attributes.Add("gendercode", new OptionSetValue(contact.gender));
+            contactEntity.Attributes.Add("familystatuscode", new OptionSetValue(contact.maritalStatus));
 
             contactEntity.Attributes.Add("address1_country", contact.country);
             contactEntity.Attributes.Add("address1_stateorprovince", contact.state);
@@ -40,23 +42,74 @@ namespace Insurance_Web
             contactEntity.Attributes.Add("address1_line2", contact.apt);
             //contactEntity.Attributes.Add("address1_telephone1", contact.phone);
             //contactEntity.Attributes.Add("emailaddress1", contact.email);
-            
-            //create the entity
-            Guid guid = service.Create(contactEntity);
+
+            c_guid = service.Create(contactEntity);
 
             return contactEntity;
         }
 
-        public Entity CreatePolicy(Policy policy)
+        public Entity CreatePolicy(Policy policy, Entity contactEntity, out Guid p_guid, Guid c_guid)
         {
+            Entity policyEntity = new Entity("new_policy");
 
-            return null;
+            policyEntity.Attributes.Add("new_policytype", new OptionSetValue(policy.type));
+            policyEntity.Attributes.Add("new_premium", new Money(Convert.ToDecimal(policy.premium)));
+            policyEntity.Attributes.Add("new_country", new OptionSetValue(policy.country));
+            policyEntity.Attributes.Add("new_policyholder", new EntityReference("contact", c_guid));
+
+            //claim.Attributes["new_contact"] = new EntityReference("contact", ((EntityReference)foundPolicy.Attributes["new_policyholder"]).Id);
+
+            p_guid = service.Create(policyEntity);
+
+            return policyEntity;
         }
 
-        public Entity CreateCar(Car car)
+        public Entity CreateCar(Car car, Entity policyEntity, Guid p_guid)
+        {
+            Entity carEntity = new Entity("new_car");
+            carEntity.Attributes.Add("new_year_text", car.year);
+            carEntity.Attributes.Add("new_car", car.make);
+            carEntity.Attributes.Add("new_model", car.model);
+            carEntity.Attributes.Add("new_policy", new EntityReference("new_policy", p_guid));
+
+            Guid guid = service.Create(carEntity);
+
+            return carEntity;
+        }
+
+        //look and see if contact exists by comparing ssn
+        public Entity getContactBySSN(string ssn)
         {
 
-            return null;
+            if (ssn == "")
+                return null;
+
+            string fetchxml = $@"
+                <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                  <entity name='contact'>
+                    <attribute name='fullname' />
+                    <attribute name='telephone1' />
+                    <attribute name='contactid' />
+                    <order attribute='fullname' descending='false' />
+                    <filter type='and'>
+                      <condition attribute='new_ssn' operator='eq' value='{ssn}' />
+                    </filter>
+                  </entity>
+                </fetch>";
+
+            EntityCollection contact = service.RetrieveMultiple(new FetchExpression(fetchxml));
+            Entity c;
+            try
+            {
+                c = contact[0];
+            }
+            catch (Exception e)
+            {
+                c = null;
+            }
+
+            return c;
         }
+        
     }
 }
